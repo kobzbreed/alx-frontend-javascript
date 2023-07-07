4732a84db5311e3c7a8f9db5af3346a2e22fcb11
kobzbreed <jitekobimowe10@gmail.com>
2023-07-07 03:01:44 +0000

    promises task done

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,4 @@
+/* eslint-disable linebreak-style */
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => resolve('Got a response from the API'));
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
