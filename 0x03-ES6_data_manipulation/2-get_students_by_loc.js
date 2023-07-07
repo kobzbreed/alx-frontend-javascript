28b45259f894502334f3061d89be4edec459a53b
kobzbreed <jitekobimowe10@gmail.com>
2023-07-07 17:33:53 +0000

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,10 @@
-/* eslint-disable*/
-export default function getStudentIdsSum(exac) {
-  const arr = Array.from(exac);
-  return arr.reduce((acc, student) => acc + student.id, 0 );
+export default function getStudentsByLocation(students, city) {
+  if (typeof city !== 'string') {
+    throw new TypeError('City must be a string');
+  }
+  if (!Array.isArray(students)) {
+    throw new TypeError('Students must be an array of objects');
+  }
+
+  return students.filter((student) => student.location === city);
 }
