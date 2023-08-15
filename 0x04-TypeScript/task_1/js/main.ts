c391691916142a6603779fb2af93a97cc388389b
jite <jitekobimowe10@gmail.com>
2023-08-15 15:03:49 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,74 +1,80 @@
 class Teacher {
-    private firstName: string;
-    private lastName: string;
-    private fullTimeEmployee: boolean;
-    private yearsOfExperience?: number;
-    private location: string;
-    private additionalAttributes: { [key: string]: any };
-  
-    constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.fullTimeEmployee = fullTimeEmployee;
-      this.location = location;
-      this.additionalAttributes = {};
-    }
-  
-    public setYearsOfExperience(yearsOfExperience: number): void {
-      this.yearsOfExperience = yearsOfExperience;
-    }
-  
-    public addAttribute(name: string, value: any): void {
-      this.additionalAttributes[name] = value;
-    }
-  
-    public getAttribute(name: string): any {
-      return this.additionalAttributes[name];
-    }
+  private firstName: string;
+  private lastName: string;
+  private fullTimeEmployee: boolean;
+  private yearsOfExperience?: number;
+  private location: string;
+  private additionalAttributes: { [key: string]: any };
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    fullTimeEmployee: boolean,
+    location: string
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullTimeEmployee = fullTimeEmployee;
+    this.location = location;
+    this.additionalAttributes = {};
   }
-  
-  interface Directors extends Teacher {
-    numberOfReports: number;
+
+  public setYearsOfExperience(yearsOfExperience: number): void {
+    this.yearsOfExperience = yearsOfExperience;
   }
-  
-  interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+
+  public addAttribute(name: string, value: any): void {
+    this.additionalAttributes[name] = value;
   }
-  
-  const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    const firstInitial = firstName.charAt(0).toUpperCase();
-    const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
-  
-    return `${firstInitial}. ${formattedLastName}`;
+
+  public getAttribute(name: string): any {
+    return this.additionalAttributes[name];
   }
-  
-  interface StudentConstructor {
-    new (firstname: string, lastName: string;): StudentClass;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const formattedLastName =
+    lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return `${firstInitial}. ${formattedLastName}`;
+};
+
+interface StudentConstructor {
+  new (firstname: string, lastName: string): StudentClass;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  
-  interface StudentInterface {
-    workOnHomework(): string;
-    displayName(): string;
+
+  workOnHomework(): string {
+    return "Currently working";
   }
-  
-  class StudentClass implements StudentInterface {
-    private firstName: string;
-    private lastName: string;
-  
-    constructor(firstName: string, lastName: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-    }
-  
-    workOnHomework(): string {
-      return "Currently working";
-    }
-  
-    displayName(): string {
-      return this.firstName;
-    }
+
+  displayName(): string {
+    return this.firstName;
   }
-  
-  const student: StudentClass = new StudentClass("John", "Doe");
-  console.log(student.workOnHomework());
-  console.log(student.displayName());
+}
+
+const student: StudentClass = new StudentClass("John", "Doe");
+console.log(student.workOnHomework());
+console.log(student.displayName());
